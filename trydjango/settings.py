7b1f5374ee13da2c076ebd5218bdfa68618e7d99
Thaileaf <lethaison2019@gmail.com>
2021-02-07 23:51:01 -0800

    changed getting email and password from json

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,5 +142,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+data = {}
+with open('debug.json') as f:
+    json.dump(data, f)
+EMAIL_HOST_USER = data['email']
+EMAIL_HOST_PASSWORD = data['email_pass']
